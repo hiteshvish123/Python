898ed39d14beb35143e45573ff87f118296fa001
Hitesh Vishwakarma <hiteshvish123@gmail.com>
2022-01-24 07:51:24 +0530

    git commit -m "fizz_buzz"

--- multiply.py
+++ multiply.py
@@ -2,8 +2,8 @@ def multiply(*numbers):
     total = 1
     for number in numbers:
         total *= number
-        return total
+    return total
 
 
 print("Start")
-print(multiply(2, 3, 4, 5))
+print(multiply(1, 2, 3))
