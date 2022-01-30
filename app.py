faca4eea05f11d749e9fe9a3d6ca37617a5b2505
Hitesh Vishwakarma <hiteshvish123@gmail.com>
2022-01-30 16:35:14 +0530

    git commit -m

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 #pylint: disable = C0114
 
-def abc(name):
-    print(f"Hi {name}")
+
+def prime_in_range():
+
+    start = int(input("Enter the starting point: "))
+    end = int(input("Enthr the last point: "))
+
+    x = 1
+
+    for x in range(start, end+1):
+        if prime_using_composite.prime(x):
+            return x
 
 
-print(abc("Hitesh"))
+print(prime_in_range())
--- prime_using_composite.py
+++ prime_using_composite.py
@@ -1,16 +1,17 @@
 import math
 
-num = int(input("Enter a number: "))
-flag = False
 
-if num>1:
-    for i in range(2,math.floor(math.sqrt(num)+1)):
-        if (num % i) == 0:
-            flag = True
+def prime(num):
+    num = int(input("Enter a number: "))
+    flag = False
+    if num > 1:
+        for i in range(2, math.floor(math.sqrt(num)+1)):
+            if (num % i) == 0:
+                flag = True
             break
 
-if flag:
-    print(num, "is not a prime number")
+    if flag:
+        return f"{num} is not a prime number"
 
-else:
-    print(num, "is a prime number")
+    else:
+        return f"{num} is a prime number"
