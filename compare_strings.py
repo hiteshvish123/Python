c964a94d09368d7de9e5401d81edaae5af6cefc2
Hitesh Vishwakarma <hiteshvish123@gmail.com>
2022-02-04 18:19:38 +0530

    git commit -m

---
+++ compare_strings.py
@@ -0,0 +1,10 @@
+#pylint: disable = C0114
+#pylint: disable = C0116
+
+input1 = input("Enter first input: ")
+input2 = input("Enter second input: ")
+
+if input1 in input2:
+    print("Match")
+else:
+    print("Do not match")
